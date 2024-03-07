52f5c091f7ead43c3ad260aa31d2d2527d467593
NCQH <66376544+NCQH@users.noreply.github.com>
2024-03-07 20:09:27 +0700

    Add files via upload

---
+++ ConcatData.m
@@ -0,0 +1,28 @@
+close all, clc
+% Ghép data lại thành 512x54144
+%% Tạo data
+datasetFolder = '';
+
+savefile = 0;
+
+load(fullfile(datasetFolder,"EEG_all_epochs.mat")); % 4514 segments
+load(fullfile(datasetFolder,"EMG_all_epochs.mat")); % 5598 segments
+
+SNRVect         = (-7:4);
+N               = numel(SNRVect);
+SNRs            = cell(1, N);
+
+for i = 1:numel(-7:-1)
+    
+    loadedData          = load(fullfile(datasetFolder, "SNR" + (i-8)+".mat"));
+    SNRs{i}             = loadedData.("SNRnegative" + (8-i));
+
+end
+for i = 0:numel(0:4)-1
+    
+    loadedData          = load(fullfile(datasetFolder, "SNR" + i +".mat"));
+    SNRs{i+8}           = loadedData.("SNR" + i);
+
+end
+numSegments         = 4512;
+allNoise = mergeAndShuff2(SNRs, (1:12));
